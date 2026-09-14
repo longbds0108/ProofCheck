159009f13f8937cc40fca84218202e0892393b09
Lê Long <lelong@MacBookPro.lan>
2026-09-14 22:29:54 +0700

    Update frontend config with deployed contract address
    Contract deployed to: 0x12F1f411f0338754F55422f29C43c70319d05080
    Network: Studio Next (61997)
    
    Frontend will now communicate with the live deployed contract.

--- dist/config.js
+++ dist/config.js
@@ -1,7 +1,7 @@
 window.PROOFCHECK_CONFIG = {
   network: 'studio-next',
   chainId: 61997,
-  contractAddress: '',
+  contractAddress: '0x12F1f411f0338754F55422f29C43c70319d05080',
   treasuryAddress: '0xf9642b695d4ddf58599c953a791f94c2e96b6a57',
   contractExplorer: 'https://explorer-studio-dev.genlayer.com/',
   rpcUrl: 'https://studio-next.genlayer.com/api',
