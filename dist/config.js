e4f3d31f25e92c56d8046bad9bfd4123b3b7c8e7
Lê Long <lelong@MacBook-Pro-cua-Le.local>
2026-09-15 11:33:56 +0700

    Switch config to chain 61999

--- dist/config.js
+++ dist/config.js
@@ -1,13 +1,14 @@
 window.PROOFCHECK_CONFIG = {
-  network: 'bradbury',
-  chainId: 4221,
-  contractAddress: '0x3cBF6C89Aa15e76A2a44881a565f657512126782',
+  network: 'studio-next',
+  chainId: 61999,
+  contractAddress: '0x...', // Paste contract address on 61999 here
   treasuryAddress: '0xf9642b695d4ddf58599c953a791f94c2e96b6a57',
-  contractExplorer: 'https://explorer.bradbury.genlayer.com/',
-  rpcUrl: 'https://rpc.bradbury.genlayer.com/api',
+  contractExplorer: 'https://explorer-studio-dev.genlayer.com/',
+  rpcUrl: 'https://studio-next.genlayer.com/api',
   rpcUrls: [
-    'https://rpc.bradbury.genlayer.com/api',
-    'https://bradbury.genlayer.com/api'
+    'https://studio-next.genlayer.com/api',
+    'https://rpc.genlayer.com/',
+    'https://api.genlayer.com/'
   ],
   sdkUrl: 'https://esm.sh/genlayer-js@2.0.0-rc.1?bundle'
 };
