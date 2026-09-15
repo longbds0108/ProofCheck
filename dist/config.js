74c5bcd6d7ea0776f84a5103a20901430443b9f1
Lê Long <lelong@MacBook-Pro-cua-Le.local>
2026-09-15 11:29:17 +0700

    Deploy ProofCheck contract on Bradbury Testnet - contract address 0x3cBF6C89Aa15e76A2a44881a565f657512126782

--- dist/config.js
+++ dist/config.js
@@ -1,7 +1,7 @@
 window.PROOFCHECK_CONFIG = {
   network: 'bradbury',
   chainId: 4221,
-  contractAddress: '0x...', // Deploy contract on chain 4221 first, then update this
+  contractAddress: '0x3cBF6C89Aa15e76A2a44881a565f657512126782',
   treasuryAddress: '0xf9642b695d4ddf58599c953a791f94c2e96b6a57',
   contractExplorer: 'https://explorer.bradbury.genlayer.com/',
   rpcUrl: 'https://rpc.bradbury.genlayer.com/api',
